95d1aecdbebb98b98a0c3547ea2d46babfd6ae6c
dbbnicole <dbbnicole>
2024-05-27 20:38:52 +0000

    Remove deployment side effect

--- files/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-project_dir = os.path.dirname(os.path.dirname(__file__))
-sys.path.append(project_dir)
-
-# this sys.path.append step is not needed if you run in a Repo, or in a Git folder with DBR 14.3+
-# this sys.path.append step is needed if you run the test locally, in the cluster terminal, or in older DBR versions.
